1a214e036300b9e8fe590970296006fec8c1de39
pdesiles <pdesiles@luccasoftware.com>
2025-03-25 15:22:34 +0100

    withUiStateAction

--- src/components/brocolist/brocolist.component.ts
+++ src/components/brocolist/brocolist.component.ts
@@ -2,11 +2,15 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { BrocoliComponent } from "../brocoli/brocoli.component";
 import { Brocoli } from "../models";
 import { BrocoliStore } from "../../stores/brocoli.store";
+import { ButtonComponent } from "@lucca-front/ng/button";
+import { LuUiStateDirective } from "@lucca/cdk/utils";
 
 @Component({
   selector: 'bro-brocolist',
   imports: [
-    BrocoliComponent
+    BrocoliComponent,
+    ButtonComponent,
+    LuUiStateDirective
   ],
   templateUrl: './brocolist.component.html',
   styleUrl: './brocolist.component.css',
--- src/stores/brocoli.store.ts
+++ src/stores/brocoli.store.ts
@@ -2,8 +2,9 @@
 import { Brocoli } from "../components/models";
 import { BrocoliApiService } from "../api/brocoli-api.service";
 import { computed, inject } from "@angular/core";
-import { withLoadingAction } from "@lucca/cdk/signal-store";
-import { onLoadingStateChange } from "@lucca/cdk/utils";
+import { withLoadingAction, withUiStateAction } from "@lucca/cdk/signal-store";
+import { onLoadingStateChange, onUiStateChange } from "@lucca/cdk/utils";
+import { delay, of } from "rxjs";
 
 export const BrocoliStore = signalStore(
   withState({
@@ -16,6 +17,17 @@ export const BrocoliStore = signalStore(
     brocoliProgress: computed(() => brocoliCount() / 100),
   })),
   withLoadingAction('init'),
+  withUiStateAction('nothing'),
+  withMethods(( store ) => ({
+    doNothingButTakeYourTime: () => {
+      of(undefined)
+        .pipe(
+          delay(2000),
+          onUiStateChange(store.setNothingActionUiState)
+        )
+        .subscribe();
+    }
+  })),
   withHooks({
     onInit(store, api = inject(BrocoliApiService)) {
       api.list$()
